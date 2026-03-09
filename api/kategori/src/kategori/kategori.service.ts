b1e2fdff5f9c1ecce01ee220e536f73c430953ce
Atri <65006655+Atri-awg@users.noreply.github.com>
2026-03-09 16:41:30 +0700

    feat(kategori): integrate PrismaService and enhance findAll method response

--- api/kategori/src/kategori/kategori.service.ts
+++ api/kategori/src/kategori/kategori.service.ts
@@ -1,15 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { CreateKategoriDto } from './dto/create-kategori.dto';
 import { UpdateKategoriDto } from './dto/update-kategori.dto';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class KategoriService {
+  // Buat constructor untuk menginisialisasi PrismaService
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createKategoriDto: CreateKategoriDto) {
     return 'This action adds a new kategori';
   }
 
-  findAll() {
-    return `This action returns all kategori`;
+  async findAll() {
+    // return this.prisma.kategori.findMany();
+    // tampilkan data kategori dengan nama dan deskripsi saja
+    const data = await this.prisma.kategori.findMany();
+    return {
+      success: true,
+      massage: '',
+      metadata: {
+        status: HttpStatus.OK,
+        total_data: data.length,
+      },
+      data: data,
+    };
   }
 
   findOne(id: number) {
