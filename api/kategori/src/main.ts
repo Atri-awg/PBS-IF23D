a2afed69314a87c96eca46f06c8626fa8b276041
Atri <65006655+Atri-awg@users.noreply.github.com>
2026-05-25 17:49:05 +0700

    feat(api): set global prefix to "api" and restrict access to localhost

--- api/kategori/src/main.ts
+++ api/kategori/src/main.ts
@@ -3,10 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // tambahkan prefix /api untuk semua route
+
+  // tambahkan prefix "api"
   app.setGlobalPrefix('api');
-  // || Falsy Operator
-  // ?? Nullish Coalescing Operator
-  await app.listen(process.env.PORT!);
+
+  // atur hanya ip yang boleh mengakses api
+  await app.listen(process.env.PORT!, 'localhost');
 }
 void bootstrap();
